b2effbdc54d576f464b39d650301ea78ea190a98
konyaru <orira4666@gmail.com>
2021-04-10 14:19:51 +0900

    途中経過：矢印キーを入力した時に計算する関数を実装途中

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import tkinter as tk
 import math
+import random
+import itertools
 
+root = None
 canvas = None
 
 SQUARE_LENGTH = 100
 RADIUS = SQUARE_LENGTH / 2 - 5
 POSITION = {"x": 8, "y": 8}
 BORDER_WIDTH = 8
-NUMBER = 4
-LENGTH = SQUARE_LENGTH * NUMBER + BORDER_WIDTH * NUMBER
+SIZE = 4
+LENGTH = SQUARE_LENGTH * SIZE + BORDER_WIDTH * SIZE
 CELL_COLOR = '#cbbeb5'
 BORDER_COLOR = '#b2a698'
 
+# fieldを作成→描画系
 def set_field():
   canvas.create_rectangle(POSITION["x"], POSITION["y"], LENGTH + POSITION["x"], LENGTH + POSITION["y"], fill='#cbbeb5', width=BORDER_WIDTH, outline=BORDER_COLOR)
 
-  for i in range(NUMBER - 1):
+  for i in range(SIZE - 1):
     x = POSITION["x"] + SQUARE_LENGTH * (i + 1) + BORDER_WIDTH * i + BORDER_WIDTH
     y = POSITION["y"] + SQUARE_LENGTH * (i + 1) + BORDER_WIDTH * i + BORDER_WIDTH
     canvas.create_line(x, POSITION["y"], x, LENGTH + POSITION["y"], width=BORDER_WIDTH, fill=BORDER_COLOR)
     canvas.create_line(POSITION["x"], y, LENGTH + POSITION["x"], y, width=BORDER_WIDTH, fill=BORDER_COLOR)
 
+# 描画系
 def create_canvas():
   root = tk.Tk()
   root.geometry(f"""{LENGTH + POSITION["x"] * 2}x{LENGTH + POSITION["y"] * 2}""")
@@ -30,22 +35,74 @@ def create_canvas():
 
   return root, canvas
 
+# 番号を描画
 def set_number(num, x, y):
   center_x = POSITION["x"] + BORDER_WIDTH * x + BORDER_WIDTH / 2 + SQUARE_LENGTH * x + SQUARE_LENGTH / 2
   center_y = POSITION["y"] + BORDER_WIDTH * y + BORDER_WIDTH / 2 + SQUARE_LENGTH * y + SQUARE_LENGTH / 2
   canvas.create_rectangle(center_x - SQUARE_LENGTH / 2, center_y - SQUARE_LENGTH / 2, center_x + SQUARE_LENGTH / 2, center_y + SQUARE_LENGTH / 2, fill=CELL_COLOR, width=0)
   canvas.create_text(center_x, center_y, text=num, justify="center", font=("", 70), tag="count_text")
 
-def operate(event):
-  print(event.keysym)
+def draw_tiles(tiles):
+  for column in range(len(tiles)):
+    for row in range(len(tiles[column])):
+      set_number(tiles[column][row].value, row, column)
 
-def play():
-  global canvas
+def move_tiles(line):
+  for i, tile in enumerate(line):
+    for j in range((i + 1), len(line)):
+      if tile(i) <= tile(j):
+
+
+# 操作系
+def operate(event, tiles):
+  print(event.keysym)
+  key_input = event.keysym
+  if key_input == 'Left':
+    for line in tiles:
+      move_tiles(line)
+
+def reset():
+  process()
+
+class Tile:
+  def __init__(self):
+    self.value = 0
+
+  def set_random_value(self):
+    self.value = random.choice([2, 4])
+
+# ここからプログラム書く
+class Game:
+  def __init__(self):
+    self.tiles = self.create_tiles()
+
+  def create_tiles(self):
+    tiles =  [[Tile() for row in range(SIZE)] for column in range(SIZE)]
+    self.set_init_value(tiles)
+    return tiles
+
+  def set_init_value(self, tiles):
+    random_tiles = random.sample(list(itertools.chain.from_iterable(tiles)), 2)
+    for tile in random_tiles:
+      tile.set_random_value()
+
+def initialize():
+  global root, canvas
   root, canvas = create_canvas()
   set_field()
-  set_number("2", 0, 3)
-  set_number("4", 2, 1)
-  root.bind("<Key>", lambda event: operate(event))
+
+def process():
+  game = Game()
+  tiles = game.tiles
+  draw_tiles(tiles)
+  # set_number("2", 0, 2)
+  # set_number("4", 2, 1)
+  root.bind("<Key>", lambda event: operate(event, tiles))
+  root.bind("<Control-Return>", lambda event: reset())
   root.mainloop()
+
+def play():
+  initialize()
+  process()
 
 play()
